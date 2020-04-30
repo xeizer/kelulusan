8cf4c8abc2d1962a4df98fdf491de03b2fa9c7f3
Teguh Firmansyah <teguhfirmansyah@Teguhs-MacBook-Pro.local>
2020-04-30 11:49:13 +0700

    ubah tampilan hal siswa

--- resources/views/halamansiswa/index.blade.php
+++ resources/views/halamansiswa/index.blade.php
@@ -1,68 +1,79 @@
 @extends('layouts.master')
 
 @section('content')
-<div class="panel panel-headline">
-    <div class="panel-heading">
-        <h3 class="panel-title">PENGUMUMAN</h3>
-        <p class="panel-subtitle">Informasi Kelulusan Siswa SMK Negeri 7 Pontianak</p>
-    </div>
-    <div class="panel-body">
-        <div class="row justify-content-center">
-            <div class="col-md-12">
-                <div class="card">
+<div class="main-content">
+    <div class="container-fluid">
+        <div id="toastr-demo" class="panel">
+            <div class="panel-body">
+                <!-- CONTEXTUAL -->
+                <div class="panel panel-headline">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">PENGUMUMAN</h3>
+                        <p class="panel-subtitle">Informasi Kelulusan Siswa SMK Negeri 7 Pontianak</p>
+                    </div>
+                    <div class="panel-body">
+                        <div class="row justify-content-center">
+                            <div class="col-md-12">
+                                <div class="card">
 
-                    <div class="card-body">
-                        @if($errors->any())
-                        <div class="alert alert-danger">{{$errors->first()}}</div>
+                                    <div class="card-body">
+                                        @if($errors->any())
+                                        <div class="alert alert-danger">{{$errors->first()}}</div>
 
-                        @endif
-                        <div class="row">
-                            <div class="col-md-3">
+                                        @endif
+                                        <div class="row">
+                                            <div class="col-md-3">
 
-                            @if(file_exists('foto/'.Auth::user()->siswa->nisn.'.jpg'))
-                            <img src="{{asset('foto/'.Auth::user()->siswa->nisn.'.jpg')}}" width="180px" />
-                            @else
-                            <img src="foto/nofoto.png" />
-                            @endif
+                                            @if(file_exists('foto/'.Auth::user()->siswa->nisn.'.jpg'))
+                                            <img src="{{asset('foto/'.Auth::user()->siswa->nisn.'.jpg')}}" width="180px" />
+                                            @else
+                                            <img src="foto/nofoto.png" />
+                                            @endif
+                                            </div>
+                                            <div class="col-md-9">
+
+                                                <p>Selamat datang di halaman Pengumuman Kelulusan SMK Negeri 7 Pontianak</p>
+                                                    <p>
+                                                        Sebelum anda mengetahui kelulusan, Anda harus menyetujui persyaratan dibawah ini.
+                                                        <ol>
+                                                            <li>Mengembalikan buku paket BOS yang dipinjam setelah disampul rapi dan buku dalam keadaan baik</li>
+                                                            <li>Tidak melakukan Pawai atau berkumpul</li>
+                                                            <li>Melaksanakan Anjuran Pemerintah mengenai Pembatasan Covid-19</li>
+                                                            <li>Tidak melakukan coret-coret seragam sekolah</li>
+                                                            <li>Seragam untuk dapat disumbangkan</li>
+                                                            <li>Lembar Surat kelulusan yang Anda download dapat anda Cetak dan di CAP di SMK Negeri 7 Pontianak</li>
+                                                            <li>Transkrip nilai menyusul</li>
+                                                        </ol>
+                                                    </p>
+                                            </div>
+                                        </div>
+
+                                        <br /><br />
+                                        <form action="{{route('cetak')}}" method="POST">
+                                            @csrf
+                                            <label>Ketik "SAYA SETUJU". jika Anda setuju untuk mematuhi peryaratan diatas</label>
+                                            <input type="text" name="setuju" class="form-control" required>
+                                            @error('pesan')
+                                            <p class="form-text text-muted text-danger">
+                                            Ketik "SAYA SETUJU". jika Anda setuju untuk mematuhi peryaratan diatas
+                                            </p>
+                                            @enderror
+                                            <input type="submit" class="btn btn-primary btn-block" value="Saya Setuju & Ingin Lihat Hasil">
+
+                                        </form>
+
+                                    </div>
+                                </div>
                             </div>
-                            <div class="col-md-9">
 
-                                <p>Selamat datang di halaman Pengumuman Kelulusan SMK Negeri 7 Pontianak</p>
-                                    <p>
-                                        Sebelum anda mengetahui kelulusan, Anda harus menyetujui persyaratan dibawah ini.
-                                        <ol>
-                                            <li>Mengembalikan buku paket BOS yang dipinjam setelah disampul rapi dan buku dalam keadaan baik</li>
-                                            <li>Tidak melakukan Pawai atau berkumpul</li>
-                                            <li>Melaksanakan Anjuran Pemerintah mengenai Pembatasan Covid-19</li>
-                                            <li>Tidak melakukan coret-coret seragam sekolah</li>
-                                            <li>Seragam untuk dapat disumbangkan</li>
-                                            <li>Lembar Surat kelulusan yang Anda download dapat anda Cetak dan di CAP di SMK Negeri 7 Pontianak</li>
-                                            <li>Transkrip nilai menyusul</li>
-                                        </ol>
-                                    </p>
-                            </div>
                         </div>
-
-                        <br /><br />
-                        <form action="{{route('cetak')}}" method="POST">
-                            @csrf
-                            <label>Ketik "SAYA SETUJU". jika Anda setuju untuk mematuhi peryaratan diatas</label>
-                            <input type="text" name="setuju" class="form-control" required>
-                            @error('pesan')
-                            <p class="form-text text-muted text-danger">
-                            Ketik "SAYA SETUJU". jika Anda setuju untuk mematuhi peryaratan diatas
-                            </p>
-                            @enderror
-                            <input type="submit" class="btn btn-primary btn-block" value="Saya Setuju & Ingin Lihat Hasil">
-
-                        </form>
-
                     </div>
                 </div>
+                <!-- END CONTEXTUAL -->
             </div>
-
         </div>
     </div>
 </div>
+
 
 @endsection
